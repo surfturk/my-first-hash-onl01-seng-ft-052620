c244d56039c3957913c4f83fa7e37461bc9f0b87
Berkley Brown <berkleydeane@gmail.com>
2020-05-29 21:49:03 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,7 +14,7 @@ def retrieval()
 end
 
 def adding
-  
+  the_manifest = {"whale bone corsets"=> 5, "porcelain vases" => 2, "oil paintings" => 3}
   # add 2 muskets to the shipping_manifest hash below
 
 
